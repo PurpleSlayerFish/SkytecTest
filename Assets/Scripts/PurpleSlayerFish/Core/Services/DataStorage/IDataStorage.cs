e44016a19e6f4a73b9e288f2ecb9e82e6121c62b
PurpleSlayerFish <alex2910sk@gmail.com>
2023-04-23 19:41:23 +0400

    Unversioned

---
+++ Assets/Scripts/PurpleSlayerFish/Core/Services/DataStorage/IDataStorage.cs
@@ -0,0 +1,9 @@
+﻿namespace PurpleSlayerFish.Core.Services.DataStorage
+{
+    public interface IDataStorage<T> where T : struct
+    {
+        void Save(T data);
+        T Load();
+        void Clear();
+    }
+}
